3e6eadad76bdbbb5154fe862366013926f87fec3
Mseza <l_gottar@orange.fr>
2018-08-02 11:33:24 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on('message', message => {
+    var prefix = '!'
+    var msg = message.content;
+    var image = msg;
+
+    if(image.charAt(0) === '!')
+    {
+      image = image.substr(1);
+    }
+
+    if (message.content.startsWith("!")) {
+        message.channel.send('', {
+            files: [
+                "./images/" + image + ".png"
+            ]
+        });
+    }
+});
+
+bot.on('ready', () => {
+    console.log("ready");
+});
+
+bot.login("NDc0MjE0NTUwODcxMjEyMDM1.DkNO2g.3glIS73ZDlbh6HWu-Xrktdq_RRM")
